6f143c856dc1f064efbb973355644510024587d3
fugel <tom@fugel.co.uk>
2020-04-26 20:11:15 +0100

    added grid layout on rotate

--- app/src/main/java/co/uk/fugel/solarsystem/Controller/MainActivity.kt
+++ app/src/main/java/co/uk/fugel/solarsystem/Controller/MainActivity.kt
@@ -1,9 +1,11 @@
 package co.uk.fugel.solarsystem.Controller
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import co.uk.fugel.solarsystem.Adapters.planetRecycleAdaptor
 import co.uk.fugel.solarsystem.R
 import co.uk.fugel.solarsystem.Services.DataService
@@ -12,21 +14,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter : planetRecycleAdaptor
+    private lateinit var adapter: planetRecycleAdaptor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = planetRecycleAdaptor(this, DataService.planets) {planet ->
+        adapter = planetRecycleAdaptor(this, DataService.planets) { planet ->
             val planetIntent = Intent(this, PlanetActivity::class.java)
             planetIntent.putExtra(EXTRA_PLANET, planet)
             startActivity(planetIntent)
         }
         planetListView.adapter = adapter
 
-//        val layoutManager = LinearLayoutManager(this)
-        val layoutManager = GridLayoutManager(this, 2)
+        val orientation = resources.configuration.orientation
+
+        val layoutManager: LinearLayoutManager
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            layoutManager = LinearLayoutManager(this)
+        } else {
+            layoutManager = GridLayoutManager(this, 3)
+        }
+
         planetListView.layoutManager = layoutManager
         planetListView.setHasFixedSize(true)
     }
